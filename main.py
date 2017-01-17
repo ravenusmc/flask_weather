03d047b314c90c56794278d23f4272836d1e7c84
ravenusmc <mcuddy77@gmail.com>
2017-01-17 11:42:55 -0500

    started work on first page

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#This is the main file for the project. 
+#This is the main file for the project.
+
+#importing all files
+from flask import Flask, render_template, request
+
+#Setting up flask
+app = Flask(__name__)
+
+
+
+#This line will actually run the app. 
+app.run(debug=True)
