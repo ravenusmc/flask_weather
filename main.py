9bc799e601b7b33c04f702a361fbb2ec8d13eb74
ravenusmc <mcuddy77@gmail.com>
2017-01-19 11:28:35 -0500

    added an about page and some styling on it

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title='Home Page')
-    
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Me')
+
 #This line will actually run the app.
 app.run(debug=True)
